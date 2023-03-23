e1eb623de67a3699405ad29d67481aaf5d71a46b
Altezi <120444635+Altezi@users.noreply.github.com>
2023-03-23 21:57:12 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = 'albert.ooo@mail.ru'
+valid_password = '862374535Djdxbr'
+empty_email = ''
+empty_password = ''
+empty_auth_key = {'key': ''}
+incorrect_auth_key = {'key': '111111111111111111111111111111111111111111111'}
